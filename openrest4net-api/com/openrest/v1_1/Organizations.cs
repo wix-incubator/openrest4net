886536fec3c7a4e24729f543d7bb6b4c81fd19c8
dleshem <dleshem@gmail.com>
2013-05-12 11:39:58 +0300

    Added GetOrganizationsRequest.

---
+++ openrest4net-api/com/openrest/v1_1/Organizations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace com.openrest.v1_1
+{
+    public class Organizations
+    {
+        public IList<Organization> organizations = new List<Organization>();
+
+        /** Empty constructor required for initialization from JSON-encoded string. */
+        public Organizations()
+        {
+        }
+
+        public Organizations(IList<Organization> organizations)
+        {
+            this.organizations = organizations;
+        }
+    }
+}
