c21a3c64c8e3b76dda793d72632b34ef0e419493
dleshem <dleshem@gmail.com>
2013-06-05 18:14:02 +0300

    Added SetOrderStatusRequest.externalIds

--- openrest4net-api/com/openrest/v1_1/SetOrderStatusRequest.cs
+++ openrest4net-api/com/openrest/v1_1/SetOrderStatusRequest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace com.openrest.v1_1
 {
@@ -11,19 +12,22 @@ namespace com.openrest.v1_1
         public string orderId;
         public string status;
         public string comment;
+        public IDictionary<string, string> externalIds = new Dictionary<string, string>();
 
         /** Empty constructor required for initialization from JSON-encoded string. */
         public SetOrderStatusRequest() : base(TYPE)
         {
         }
 
-        public SetOrderStatusRequest(string accessToken, string confirmationToken, string orderId, string status, string comment) : base(TYPE)
+        public SetOrderStatusRequest(string accessToken, string confirmationToken,
+            string orderId, string status, string comment, IDictionary<string, string> externalIds) : base(TYPE)
         {
             this.accessToken = accessToken;
             this.confirmationToken = confirmationToken;
             this.orderId = orderId;
             this.status = status;
             this.comment = comment;
+            this.externalIds = externalIds;
         }
     }
 }
