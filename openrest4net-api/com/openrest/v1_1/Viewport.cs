41cb0203277a5e585a017e882a7007fb8b2afa01
dleshem <dleshem@gmail.com>
2013-10-29 12:53:02 +0200

    Added GetOrderRequest

---
+++ openrest4net-api/com/openrest/v1_1/Viewport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace com.openrest.v1_1
+{
+    public class Viewport
+    {
+        public int? width;
+        public double? initialScale;
+        public double? maximumScale;
+
+        public Viewport(int width, double initialScale, double maximumScale)
+        {
+            this.width = width;
+            this.initialScale = initialScale;
+            this.maximumScale = maximumScale;
+        }
+
+        /** Empty constructor required for initialization from JSON-encoded string. */
+        public Viewport() { }
+    }
+}
