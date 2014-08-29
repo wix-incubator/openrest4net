1fd887c55c5965987bf132826ce478c09c6f8e1f
3LOK <yoavamit@yahoo.com>
2014-08-29 01:47:51 -0700

    Added GetAppMappedObject.

---
+++ openrest4net-api/com/openrest/v1_1/AppId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace com.openrest.v1_1
+{
+
+    public class AppId
+    {
+        public AppId(string platform, string id, string version)
+        {
+            this.platform = platform;
+            this.id = id;
+            this.version = version;
+        }
+
+        /** Empty constructor required for initialization from JSON-encoded string. */
+        public AppId() { }
+
+        public string platform;
+        public string id;
+        public string version;
+    }
+}
